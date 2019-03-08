d807b6645a9f2197185779132a337e8466c2daf6
dupontm <martin.dupont@tngtech.com>
2019-03-08 16:06:45 +0100

    * Added cloudformation deploy step * Removed unnecessary files

--- cicd/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WEB_BUCKET=$(aws cloudformation describe-stack-resource --stack-name $DISTRIBUTION_STACK_NAME --logical-resource-id ContentBucket --output text --query StackResourceDetail.PhysicalResourceId)
-aws s3 sync ./build s3://$WEB_BUCKET/ --exclude index.html max-age=100000
-aws s3 cp ./build/index.html s3://$WEB_BUCKET/ --cache-control max-age=20
