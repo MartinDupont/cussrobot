88d754178bffafd0be6441b9bb8c570a89a7745b
dupontm <martin.dupont@tngtech.com>
2019-03-09 22:50:31 +0100

    basic attempt at venv

--- build.sh
+++ build.sh
@@ -6,11 +6,10 @@ mkdir -p tmp/venv
 mkdir -p tmp/package
 
 
-cp src/ tmp/package/
+cp src/* tmp/package/
 python3 -m venv tmp/venv
 source tmp/venv/bin/activate
-pip3 install -r python-twitter
+pip3 install python-twitter
 
-
-
+cp -r tmp/venv/lib/python3.6/site-packages/. tmp/package/
 chmod -R 777 tmp
--- src/index.py
+++ src/index.py
@@ -1,4 +1,5 @@
 import random
+import twitter
 
 def lambda_handler(event_json, context):
     tweet_text = "Random tweet number: {}".format(random.randint(0, 10000))
