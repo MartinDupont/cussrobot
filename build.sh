41eac5accdbea2c6f2e868b3300ffa276dd5f34c
dupontm <martin.dupont@tngtech.com>
2019-03-10 10:13:18 +0100

    * Fixed local package imports in build step. * Added basic use of twitter package.

--- build.sh
+++ build.sh
@@ -2,14 +2,10 @@
 
 rm -rf tmp
 [[ -d tmp ]] && { echo "Please delete tmp folder before build."; exit 1; }
-mkdir -p tmp/venv
 mkdir -p tmp/package
 
 
-cp src/* tmp/package/
-python3 -m venv tmp/venv
-source tmp/venv/bin/activate
-pip3 install python-twitter
+cp src/* tmp/
+pip3 install python-twitter --target tmp
 
-cp -r tmp/venv/lib/python3.6/site-packages/. tmp/package/
 chmod -R 777 tmp
--- src/index.py
+++ src/index.py
@@ -1,5 +1,16 @@
 import random
 import twitter
+import os
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+api = twitter.Api(consumer_key=CONSUMER_KEY,
+                  consumer_secret=CONSUMER_SECRET,
+                  access_token_key=ACCESS_TOKEN_KEY,
+                  access_token_secret=ACCESS_TOKEN_SECRET)
 
 def lambda_handler(event_json, context):
     tweet_text = "Random tweet number: {}".format(random.randint(0, 10000))
