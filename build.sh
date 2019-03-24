22b732ad31be4b1782d0475273da48669ca85aff
Martin <m.dupont@live.com.au>
2019-03-24 21:33:02 +0100

    added numpy install and database setup to build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ rm -rf tmp
 [[ -d tmp ]] && { echo "Please delete tmp folder before build."; exit 1; }
 mkdir -p tmp/package
 
+python3 setup/make_processed_database.py
 
 cp src/* tmp/
-pip3 install python-twitter --target tmp
+pip3 install numpy python-twitter --target tmp
 
 chmod -R 777 tmp
