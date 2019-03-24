60faeac7664a4c5d25c3538bbfff6612d5e0e263
Martin <m.dupont@live.com.au>
2019-03-24 21:41:59 +0100

    added pandas install to build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ rm -rf tmp
 [[ -d tmp ]] && { echo "Please delete tmp folder before build."; exit 1; }
 mkdir -p tmp/package
 
+pip3 install pandas
 python3 setup/make_processed_database.py
 
 cp src/* tmp/
 pip3 install numpy python-twitter --target tmp
 
+ls tmp
 chmod -R 777 tmp
