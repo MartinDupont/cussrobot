196f5b0b637cd207b6a8b52646d41eb9a6d36041
Martin <m.dupont@live.com.au>
2019-03-25 21:00:33 +0100

    improved test coverage

--- src/make_tweets/test_tweets.py
+++ src/make_tweets/test_tweets.py
@@ -1,6 +1,6 @@
 import unittest
 
-from .make_tweets import make_tweet, tidy_grammar
+from .make_tweets import make_tweet, tidy_grammar, pick_random, sub_emojis, sub_hashtags
 
 class CheckTweets(unittest.TestCase):
 
@@ -23,6 +23,30 @@ class CheckTweets(unittest.TestCase):
             result = tidy_grammar(given)
             self.assertEqual(result, expected)
         
-
+    def test_pick_random(self):
+        """should return an element from the list"""
+        probs = {"a":0.5, "b":0.5}
+        for i in range(10):
+            result = pick_random(probs)
+            self.assertTrue(result in probs.keys())
+ 
+    def test_pick_random_2(self):
+        """should handle the trivial case"""
+        probs = {"a":1}
+        result = pick_random(probs)
+        self.assertTrue(result == "a")
+           
+    def test_sub_emojis(self):
+        input_string = "yo this is bad EMOJIHERE "
+        distribution = {"&#128514;": 1}
+        result = sub_emojis(input_string, distribution)
+        self.assertEqual(result, "yo this is bad 😂")
+    
+    def test_sub_hashtags(self):
+        input_string = "yo this is bad HASHTAGHERE "
+        distribution = {"#YOLO": 1}
+        result = sub_hashtags(input_string, distribution)
+        self.assertEqual(result, "yo this is bad #YOLO ")    
+    
 if __name__ == "__main__":
     unittest.main()
